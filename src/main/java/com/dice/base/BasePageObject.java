f7c9cb1d5e489a9a5035264170dac30ef5e880a1
Panks1 <pankul.1991.2@gmail.com>
2018-05-03 00:37:51 +0530

    Added WebDriver Wait

--- src/main/java/com/dice/base/BasePageObject.java
+++ src/main/java/com/dice/base/BasePageObject.java
@@ -1,4 +1,15 @@
 package com.dice.base;
 
-public class BasePageObject {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePageObject<T> {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+
+    protected BasePageObject(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver,40);
+    }
 }
