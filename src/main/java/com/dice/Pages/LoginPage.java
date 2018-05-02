baf65463894a8196efb327caa9c1ddc149c933f1
Panks1 <pankul.1991.2@gmail.com>
2018-05-03 00:41:59 +0530

    Added Login Page and Profile Page

---
+++ src/main/java/com/dice/Pages/LoginPage.java
@@ -0,0 +1,11 @@
+package com.dice.Pages;
+
+import com.dice.base.BasePageObject;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePageObject<LoginPage> {
+
+    protected LoginPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/main/java/com/dice/Pages/ProfilePage.java
@@ -0,0 +1,11 @@
+package com.dice.Pages;
+
+import com.dice.base.BasePageObject;
+import org.openqa.selenium.WebDriver;
+
+public class ProfilePage extends BasePageObject<ProfilePage> {
+
+    protected ProfilePage(WebDriver driver) {
+        super(driver);
+    }
+}
